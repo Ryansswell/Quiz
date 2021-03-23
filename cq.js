4a97850ddd5186f9224b908796bbf0a2390b77fc
Ryan Anderson <ryan0425@msn.com>
2021-03-22 21:44:54 -0700

    Errors fixed on Console

--- cq.js
+++ cq.js
@@ -6,31 +6,54 @@ var startButton = document.querySelector("#start");
 
 var myQuestion = [
     {
-        question: "##",
+        question: "Commonly used data types DO NOT include:",
         choices: {
-            a: "<scripting>",
-            b: "<script>",
-            c: "<javascript>",
+            a: "<strings>",
+            b: "<booleans>",
+            c: "<alerts>",
+            d: "<number>"
         },
-        correctAnswer: "b"
+        correctAnswer: "c"
     },
     {
-        question: "##",
+        question: "The condition in an if/else statement",
         choices: {
-            a: "<scripting>",
-            b: "<script>",
-            c: "<javascript>",
+            a: "<quotes>",
+            b: "<curly brackets>",
+            c: "<parentheses>",
+            d: "<square brackets>",
         },
-        correctAnswer: "b"
+        correctAnswer: "c"
     },
     {
-        question: "##",
+        question: "Arrays in JavaScript can be used to store _____.",
         choices: {
-            a: "<scripting>",
-            b: "<script>",
-            c: "<javascript>",
+            a: "<numbers and strings>",
+            b: "<other arrays>",
+            c: "<booleans>",
+            d: "<all the above>",
         },
-        correctAnswer: "b"
+        correctAnswer: "d"
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: {
+            a: "<commas>",
+            b: "<curly brackets>",
+            c: "<quotes>",
+            d: "<parentheses>",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: {
+            a: "<JavaScript>",
+            b: "<Terminal/Bash>",
+            c: "<for loops>",
+            d: "<console.log>",
+        },
+        correctAnswer: "d"
     }
 ];
 
@@ -42,32 +65,35 @@ startButton.addEventListener("click",function(event) {
 
 
     var choices = new Array(myQuestion[index].choices);
+    console.log(choices[0].a);
 
     questionHeader.textContent=(myQuestion[index].question);
 
-    for (i=0; i < choices.length; i++) {
-        var answerChoices = (JSON.stringify(choices[i]));
-        var A = document.createElement("Button");
-        A.textContent = (JSON.parse(answerChoices).a);
-        var B = document.createElement("Button");
-        B.textContent = (JSON.parse(answerChoices).b);
-        var C = document.createElement("Button");
-        C.textContent = (JSON.parse(answerChoices).c);
+    var A = document.createElement("Button");
+    A.textContent = choices[0].a;
 
-        questionBody.appredChild(A);
+    //for (i=0; i < choices.length; i++) {
+       // var answerChoices = (JSON.parse(choices[i]));
+       // var A = document.createElement("Button");
+        // A.textContent = (JSON.parse(answerChoices).a);
+        // var B = document.createElement("Button");
+        // B.textContent = (JSON.parse(answerChoices).b);
+        // var C = document.createElement("Button");
+        // C.textContent = (JSON.parse(answerChoices).c);
 
-        var br = document.createElement("br");
-        questionBody.appendChild(br);
+    //     questionBody.appredChild(A);
 
-        questionBody.appredChild(B);
+    //     var br = document.createElement("br");
+    //     questionBody.appendChild(br);
 
-        var br = document.createElement("br");
-        questionBody.appendChild(br);
+    //     questionBody.appredChild(B);
 
-        questionBody.appredChild(C);
+    //     var br = document.createElement("br");
+    //     questionBody.appendChild(br);
 
-        var br = document.createElement("br");
-        questionBody.appendChild(br);
-    }
-}
+    //     questionBody.appredChild(C);
 
+    //     var br = document.createElement("br");
+    //     questionBody.appendChild(br);
+    // }
+})
