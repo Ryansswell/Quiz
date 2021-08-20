f9ed9557818cb66e0ce07ac4441b96847141711e
Ryan Anderson <ryan0425@msn.com>
2021-08-20 14:13:41 -0700

    Updated the Highscores screen and functionality, along with updating a lot of the styles.css

---
+++ assets/js/questions.js
@@ -0,0 +1,61 @@
+var questions = [
+    {
+        title: "Which array method sorts the elements of an array?",
+        choices: ["sort()", "changeOrder(order)", "order()", "None of the above"],
+        answer: "sort()"
+    },
+    {
+        title: "Commonly used data types DO NOT include?",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Which function is used to load the next URL in the history list?",
+        choices: ["window.history.next()", "window.history.load_next()", "window.history.forward()", "window.history.load_forward()"],
+        answer: "window.history.forward()"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "How do you get cookies in JavaScript?",
+        choices: ["window.cookies", "location.cookies", "document.cookie", "document.cookies"],
+        answer: "window.cookie"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        title: "Which of the following is used for assigning a value to a variable?",
+        choices: ["*", "=", "-", "X"],
+        answer: "="
+    },
+    {
+        title:
+            "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "MERN Stands for?",
+        choices: ["Managing External Remote Networking", "Micro Effecient Responsive Node",
+            "MongoDB, Express, REACT, Node",
+            "Modular Remote Network"],
+        answer: "MongoDB, Express, REACT, Node"
+    },
+    {
+        title:
+            "A very useful tool used during development and debugging for printing content to the debugger is?",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
